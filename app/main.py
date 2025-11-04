89b48dd077e66489a8d0f9185db3fa9a24d47ffc
W Mirshod <python3526@gmail.com>
2025-11-05 03:07:51 +0500

    Update docker-compose configuration and enhance FastAPI request handling. Changed the port mapping in docker-compose.yml to use the default port 8000. Updated the read_root function in main.py to explicitly type the request parameter as Request for better clarity and type checking.

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI(title="Time Series Graphing")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
